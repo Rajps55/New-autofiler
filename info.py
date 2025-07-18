077f2db97ea57bf934bd6c7f064a375b68de4eec
Rajps55 <hackinggm69@gmail.com>
2025-07-18 20:57:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,7 +86,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 # Links
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Netfilix_movie_shaport')
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/+Ie4mNf6lL5RjYjM1')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/n_flixmovie')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/botlogpy')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/APSmo/7")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/APSmo/7")
 
